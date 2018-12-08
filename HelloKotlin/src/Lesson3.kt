52b86190a57f5832c024524a77a2961732971181
Thao Nguyen <thaonguyen.au1@gmail.com>
2018-12-08 21:25:30 +0700

    Done lesson 3

--- HelloKotlin/src/Lesson3.kt
+++ HelloKotlin/src/Lesson3.kt
@@ -17,6 +17,11 @@ fun main(args: Array<String>) {
     swim(5)
     swim(5,"slow")
     swim(speed = "slow", time = 5)
+
+    println(whatShouldIDoToday("happy"))
+
+    var swim = { println("swim \n")}
+    swim()
 }
 
 fun dayOfWeek() {
@@ -35,8 +40,26 @@ fun dayOfWeek() {
 }
 
 fun shouldChangeWater(day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
-    return true
+//    return when {
+//        temperature > 30 -> true
+//        dirty > 30 -> true
+//        day == "Sunday" -> true
+//        else -> false
+//    }
+
+    return when {
+        isTooHot(temperature) -> true
+        isDirty(dirty) -> true
+        isSunday(day) -> true
+        else -> false
+    }
 }
+
+fun isTooHot(temperature: Int) = temperature > 30
+
+fun isDirty(dirty: Int) = dirty > 30
+
+fun isSunday(day: String) = day == "Sunday"
 
 fun feedTheFish() {
     val day = randomDay()
@@ -73,4 +96,27 @@ fun fishFood(day: String): String {
 
 fun swim(time: Int, speed: String = "fast") {
     println("swimming $speed")
+}
+
+fun whatShouldIDoToday(mood: String, weather: String = "sunny", temperature: Int = 24): String {
+    return when {
+        mood == "happy" && weather == "sunny" -> "go for a walk"
+        else -> "Stay home and read"
+    }
+}
+
+//lambdas
+var dirty = 20
+val waterFilter: (Int) -> Int = { dirty -> dirty / 2}
+fun feedFish(dirty: Int) = dirty + 10
+fun updateDirty(dirty: Int, operation: (Int) -> Int): Int {
+    return operation(dirty)
+}
+
+fun dirtyProcessor(){
+    dirty = updateDirty(dirty, waterFilter)
+    dirty = updateDirty(dirty, ::feedFish)
+    dirty = updateDirty(dirty) { dirty ->
+        dirty + 50
+    }
 }
