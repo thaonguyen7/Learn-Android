79aa2c5161deda930acd97d87e041a889e1c3daf
Thao Nguyen <thaonh.ict7@gmail.com>
2019-01-31 00:00:19 +0700

    learn mvp architecture

---
+++ umbrella-mvp/app/src/main/java/com/raywenderlich/android/rwandroidtutorial/MainPresenter.kt
@@ -0,0 +1,35 @@
+package com.raywenderlich.android.rwandroidtutorial
+
+class MainPresenter(view: MainContract.View, dependencyInjector: DependencyInjector)
+    : MainContract.Presenter {
+
+    private val weatherRepository: WeatherRepository = dependencyInjector.weatherRepository()
+
+    private var view: MainContract.View? = view
+
+    private fun loadWeather() {
+        val weather = weatherRepository.loadWeather()
+        val weatherState = weatherStateForWeather(weather)
+
+        view?.displayWeatherState(weatherState)
+    }
+
+    private fun weatherStateForWeather(weather: Weather): WeatherState {
+        if (weather.rain?.amount ?: 0 > 0) {
+            return WeatherState.RAIN
+        }
+        return WeatherState.SUN
+    }
+
+    override fun onDestroy() {
+        this.view = null
+    }
+
+    override fun onViewCreated() {
+        loadWeather()
+    }
+
+    override fun onLoadWeatherTapped() {
+        loadWeather()
+    }
+}
