a1ea6c948e4850aa865de5453a80cc007fb4822c
Thao Nguyen <thaonh@sigma-solutions.eu>
2019-02-25 17:49:48 +0700

    Dagger 2 sample

---
+++ droidwiki/app/src/main/java/com/raywenderlich/android/droidwiki/dagger/AppComponent.kt
@@ -0,0 +1,11 @@
+package com.raywenderlich.android.droidwiki.dagger
+
+import com.raywenderlich.android.droidwiki.ui.homepage.HomepageActivity
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [AppModule::class, PresenterModule::class])
+interface AppComponent {
+    fun inject(target: HomepageActivity)
+}
---
+++ droidwiki/app/src/main/java/com/raywenderlich/android/droidwiki/dagger/PresenterModule.kt
@@ -0,0 +1,14 @@
+package com.raywenderlich.android.droidwiki.dagger
+
+import com.raywenderlich.android.droidwiki.ui.homepage.HomepagePresenter
+import com.raywenderlich.android.droidwiki.ui.homepage.HomepagePresenterImpl
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class PresenterModule {
+    @Provides
+    @Singleton
+    fun provideHomepagePresenter(): HomepagePresenter = HomepagePresenterImpl()
+}
