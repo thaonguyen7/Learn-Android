26bf7d8a43c9dc76783d30f477cccf40bbf231a5
Thao Nguyen <thaonh.ict7@gmail.com>
2019-06-21 16:28:02 +0700

    Room sample

---
+++ RoomWordSample/app/src/main/java/com/bluesky/roomwordsample/WordDao.kt
@@ -0,0 +1,19 @@
+package com.bluesky.roomwordsample
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface WordDao {
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    fun getAllWords(): LiveData<List<Word>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    @Query("DELETE FROM word_table")
+    fun deleteAll()
+}
